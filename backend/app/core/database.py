01d43b02d30f0829564de5c0ab41aed2aa8a008b
Raushan Kumar <24f2000297@ds.study.iitm.ac.in>
2026-06-27 17:57:42 +0530

    postgreSQL database is enabled

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -3,10 +3,13 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 from app.core.config import DATABASE_URL
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False},
+    )
+else:
+    engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
